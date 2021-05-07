ceb8e5ebba5e3edc1260f2ed1f7c02049317b512
thirstyice <thirstyice@users.noreply.github.com>
2021-05-06 21:03:15 -0600

    Add blackouts backend

--- modules/blackouts.js
+++ modules/blackouts.js
@@ -1,8 +1,19 @@
+const SerialPort = require("serialport");
+const path = ""
+//const serialport = new SerialPort(path, {baudRate: 9600, dataBits: 8, stopBits: 1, parity: 'none'}
+
 exports.open = function (curtainId) {
 	console.log("Opening blackout: " + curtainId);
-
+	serialport.send("#DEVICE,1," + curtainId + ",18");
 };
 exports.close = function (curtainId) {
 	console.log("Opening blackout: " + curtainId);
-
+	serialport.send("#DEVICE,1," + curtainId + ",19");
 };
+exports.stop = function (curtainId) {
+	console.log("Stopping blackout: " + curtainId);
+	serialport.send("#DEVICE,1," + curtainId + ",20");
+}
+exports.recallPosition(curtainId) {
+	serialport.send("#DEVICE,1," + curtainId + ",3");
+}
