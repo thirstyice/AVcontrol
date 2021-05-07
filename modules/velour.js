1eece0694cbd3395cd6fc80eb3eb8a93fda8803e
thirstyice <thirstyice@users.noreply.github.com>
2021-05-06 19:14:38 -0600

    Finish basic blind control

--- modules/velour.js
+++ modules/velour.js
@@ -1,6 +1,16 @@
+const SerialPort = require("serialport");
+const path = ""
+//const serialport = new SerialPort(path, {baudRate: 9600, dataBits: 8, stopBits: 1, parity: 'none'});
+
 exports.open = function (curtainId) {
-console.log("Opening velour: " + curtainId);
+	console.log("Opening velour: " + curtainId);
+	serialport.write("01" + curtainId.padStart(2,"0") + "U");
 };
 exports.close = function (curtainId) {
-console.log("Closing velour: " + curtainId);
+	console.log("Closing velour: " + curtainId);
+	serialport.write("01" + curtainId.padStart(2,"0") + "D");
+};
+exports.close = function (curtainId) {
+	console.log("Stopping velour: " + curtainId);
+	serialport.write("01" + curtainId.padStart(2,"0") + "S");
 };
