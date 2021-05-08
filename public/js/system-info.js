58594111729143599a440db3747b321385b850bc
thirstyice <thirstyice@users.noreply.github.com>
2021-05-08 14:22:44 -0600

    Add capability for multi-word keys in system-info

--- public/js/system-info.js
+++ public/js/system-info.js
@@ -11,8 +11,8 @@ socket.on("set-system-info", (info) => {
 	var table = document.getElementById("info");
 	for (const [key, value] of Object.entries(info)) {
 		var row = document.createElement("tr");
-		row.appendChild(document.createElement("td")).innerText = capFirstLetter(`${key}`);
-		row.appendChild(document.createElement("td")).innerText = capFirstLetter(`${value}`);
+		row.appendChild(document.createElement("td")).innerText = capFirstLetter(`${key}:`).replace(/_/g, ' ');
+		row.appendChild(document.createElement("td")).innerText = capFirstLetter(`${value}`).replace(/_/g, ' ');
 		table.appendChild(row);
 		console.log(`${key}: ${value}`);
 	};
