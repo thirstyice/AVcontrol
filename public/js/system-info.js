c2d717d35525af5c77efcaadbf91fed060b3af8d
thirstyice <thirstyice@users.noreply.github.com>
2021-05-08 13:40:32 -0600

    Expand the capabilities of system-info
    Now what kinds of info are printed are handled by the server

--- public/js/system-info.js
+++ public/js/system-info.js
@@ -3,15 +3,19 @@ const socket = io();
 socket.on("disconnect", (reason) => {
 	console.log("Socket disconnected: " + reason);
 });
-
-function setInfo(id, info) {
-	var node = document.getElementById(id).appendChild(document.createElement("td"));
-	node.innerHTML = info;
+function capFirstLetter(string) {
+	return string.replace(/^\w/, (c) => c.toUpperCase());
 }
 
-socket.on("set-system-info", (arg) => {
-	setInfo("version", arg.version);
-	setInfo("clients", arg.clients);
+socket.on("set-system-info", (info) => {
+	var table = document.getElementById("info");
+	for (const [key, value] of Object.entries(info)) {
+		var row = document.createElement("tr");
+		row.appendChild(document.createElement("td")).innerText = capFirstLetter(`${key}`);
+		row.appendChild(document.createElement("td")).innerText = capFirstLetter(`${value}`);
+		table.appendChild(row);
+		console.log(`${key}: ${value}`);
+	};
 });
 
 window.onload = function () {
