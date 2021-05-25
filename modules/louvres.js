e42a3b15eb23ac1f86df61f3eee5d0a53fef3dcb
thirstyice <thirstyice@users.noreply.github.com>
2021-05-24 18:25:53 -0600

    Add louvres backend

--- modules/louvres.js
+++ modules/louvres.js
@@ -7,23 +7,23 @@ port.on('error', (err) => {
 	console.error("Screens: " + err);
 });
 
-function sendCommand(command, space) {
+function sendCommand(command) {
 	console.info("Louvres: sending: " + command)
-	port.write(command);
+	port.write(command + "\r\n");
 }
 
 exports.open = function (group) {
-
+	sendCommand(":" + group + "060001004B--");
 }
 exports.close = function (group) {
-
+	sendCommand(":" + group + "060001004E--");
 }
 exports.stop = function (group) {
-
+	sendCommand(":" + group + "0600010028--");
 }
 exports.tiltOpen = function (group) {
-
+	sendCommand(":" + group + "060001001A--");
 }
 exports.tiltClose = function (group) {
-
+	sendCommand(":" + group + "0600010016--");
 }
